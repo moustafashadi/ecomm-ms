63ce857068346b2cdfbe071be73d0a40652ab74d
Moustafa Mahmoud Shadi <moustafa.mahmoud@student.giu-uni.de>
2024-07-15 12:52:27 +0300

    start of user auth

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/users", userRoutes);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
