7e08d2c3b58c49e1be9058c05d27d309ee74f4fb
Moustafa Mahmoud Shadi <95936574+shadisvtwin@users.noreply.github.com>
2024-10-06 17:51:08 +0300

    6/10/24

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { 
     createUser, 
     loginUser , 
-    logoutCurrentUser, 
+    logoutCurrentUser,
     getAllUsers ,
     getCurrentUserProfile,
     updateCurrentUserProfile,
